537ffe5bb0d7d7a186ed7ed8b3e478cc52742602
Rui Silva <visitfoods@gmail.com>
2025-04-01 11:08:53 +0100

    Ajustar configuração do Next.js e adicionar redirecionamento 404

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/IQOSILUMAi' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/IQOSILUMAi/' : '',
+  basePath: '/IQOSILUMAi',
+  assetPrefix: '/IQOSILUMAi/',
   images: {
     unoptimized: true,
     domains: ['visitfoods.github.io']
