146d15fef9e74a827fcf1284b9f284e3960695a5
Rui Silva <visitfoods@gmail.com>
2025-04-01 11:00:43 +0100

    Ajustar configuração para GitHub Pages e adicionar .nojekyll

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/IQOSILUMAi',
-  assetPrefix: '/IQOSILUMAi/',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true
   },
