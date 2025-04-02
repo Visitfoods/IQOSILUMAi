2dd64909cb7df09e8f09b69e3ca123ee3c59605b
Rui Silva <visitfoods@gmail.com>
2025-04-02 17:14:10 +0100

    Corrigir caminhos das imagens para ILUMAi ONE e PRIME

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -16,8 +16,8 @@ type ColorVariant = "Breeze" | "Midnight" | "Leaf" | "Terracotta" | "Violet";
 
 const machines: Machine[] = [
   { id: 1, name: "ILUMAi BREEZE", image: "/IMG/ILUMAi/ILUMAi_BREEZE.png", baseModel: "ILUMAi" },
-  { id: 2, name: "ILUMAi ONE", image: "/IMG/ILUMAi/ILUMAi_ONE.png", baseModel: "ILUMAi-ONE" },
-  { id: 3, name: "ILUMAi PRIME", image: "/IMG/ILUMAi/ILUMAi_PRIME.png", baseModel: "ILUMAi-PRIME" },
+  { id: 2, name: "ILUMAi ONE", image: "/IMG/ILUMAi-ONE/ILUMAi-ONE_BREEZE.png", baseModel: "ILUMAi-ONE" },
+  { id: 3, name: "ILUMAi PRIME", image: "/IMG/ILUMAi-PRIME/ILUMAi-PRIME_BREEZE.png", baseModel: "ILUMAi-PRIME" },
 ];
 
 const colorConfig = [
@@ -110,7 +110,7 @@ export default function Carousel() {
     if (colorVariant === "Terracotta") {
       colorForPath = "TERRACOTA";
     } else if (colorVariant === "Violet") {
-      colorForPath = "-VIOLET";
+      colorForPath = "VIOLET";
     }
     return `/IMG/${machine.baseModel}/${machine.baseModel}_${colorForPath}.png`;
   };
