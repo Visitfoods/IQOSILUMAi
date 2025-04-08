37bc7cbe9e1f0d5e54a72d1f3711b98612a6f069
Rui Silva <visitfoods@gmail.com>
2025-04-08 02:44:36 +0100

    Remover o DotPattern completamente

--- src/components/DotPattern.tsx
+++ src/components/DotPattern.tsx
@@ -1,25 +1,7 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export default function DotPattern() {
-  useEffect(() => {
-    console.log('DotPattern montado - Ajustando para posição intermediária');
-  }, []);
-
-  return (
-    <div className="fixed inset-0 w-screen h-screen z-20 pointer-events-none">
-      {/* Imagem como background */}
-      <div 
-        className="absolute inset-0 opacity-80"
-        style={{
-          backgroundImage: 'url(/Anel.png)',
-          backgroundSize: '105% 110%',
-          backgroundPosition: 'center top',
-          backgroundRepeat: 'no-repeat',
-          transform: 'scaleY(1.05) scaleX(1.02) translateY(-10%)',
-        }}
-      />
-    </div>
-  );
+  return null; // Retornando null para não renderizar nada
 } 
